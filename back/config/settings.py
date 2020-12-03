ff897db7bd27511d854e82bb2aded56818b56c05
Peter-Roh <rmc2@naver.com>
2020-12-03 17:13:48 +0900

    project setting

--- back/config/settings.py
+++ back/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = ["localhost", "http://projectsetting.eba-3txh2g2y.ap-northeast-2.elasticbeanstalk.com/", "projectsetting.eba-3txh2g2y.ap-northeast-2.elasticbeanstalk.com",]
+ALLOWED_HOSTS = ["localhost", "http://projectsetting.eba-3txh2g2y.ap-northeast-2.elasticbeanstalk.com/",]
 
 # Application definition
 
