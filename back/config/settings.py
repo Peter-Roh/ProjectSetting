3f3996d2af3639a764b03cd041ac685bf59601bd
Peter-Roh <rmc2@naver.com>
2020-12-05 21:54:33 +0900

    project setting

--- back/config/settings.py
+++ back/config/settings.py
@@ -189,7 +189,7 @@ elif STATE == "dev":
     AWS_BUCKET_ACL = 'public-read'
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+#    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
 # elif STATE == "production":
 #     DEBUG = False
